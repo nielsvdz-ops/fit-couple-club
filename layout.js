3b1dbd98c95730cdcb6ca3ec68aa4b2867958e21
nielsvdz-ops <nielsvdz@gmail.com>
2026-03-19 19:22:31 +0800

    Add files via upload

---
+++ layout.js
@@ -0,0 +1,13 @@
+
+export const metadata = {
+  title: 'Fit Couple Club',
+  description: 'Fitness platform'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
