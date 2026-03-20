289e220d3b90da8b483bb88eebaed2ccf27b72ad
nielsvdz-ops <nielsvdz@gmail.com>
2026-03-20 09:39:30 +0800

    Create page.js

---
+++ app/nutrition/page.js
@@ -0,0 +1,8 @@
+export default function WorkoutsPage() {
+  return (
+    <main style={{ minHeight: "100vh", padding: "40px 24px", background: "#0a0a0a", color: "white" }}>
+      <h1>Workouts</h1>
+      <p>Here you will place the 70 workout options.</p>
+    </main>
+  );
+}
