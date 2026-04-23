40c282249d30c88da31785a606b0e6b220839f79
nielsvdz-ops <nielsvdz@gmail.com>
2026-04-23 08:35:44 +0800

    Update page.js

--- app/recipes/page.js
+++ app/recipes/page.js
@@ -3,14 +3,14 @@ export const dynamic = "force-dynamic";
 import { redirect } from "next/navigation";
 import DashboardLayout from "../../components/DashboardLayout";
 import { getCurrentUserAndProfile } from "../../lib/getProfile";
-import { canAccessStarterPages } from "../../lib/access";
+import { canAccessNutritionPages } from "../../lib/access";
 import RecipesClient from "../../components/RecipesClient";
 
 export default async function RecipesPage() {
   const { user, profile } = await getCurrentUserAndProfile();
 
   if (!user) redirect("/login");
-  if (!canAccessStarterPages(profile)) redirect("/pricing");
+  if (!canAccessNutritionPages(profile)) redirect("/billing");
 
   return (
     <DashboardLayout
